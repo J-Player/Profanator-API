c966d57a2a3cf95c2d100f197bbd81b2a342da10
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-01-06 15:14:36 -0300

    IntegrationConstraint atualizado.

--- src/test/java/api/integration/constraint/IntegrationConstraint.java
+++ src/test/java/api/integration/constraint/IntegrationConstraint.java
@@ -7,4 +7,8 @@ public abstract class IntegrationConstraint {
     public static final String ADMIN_AUTHORITIES = "ROLE_ADMIN,ROLE_USER";
     public static final String USER_AUTHORITIES = "ROLE_USER";
 
+    public static final String PATH_PROFICIENCIES = "/proficiencies";
+    public static final String PATH_ITEMS = "/items";
+    public static final String PATH_INGREDIENTS = "/ingredients";
+
 }
