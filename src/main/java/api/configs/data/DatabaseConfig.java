7146184a87125e46f18e30b67e5d5217eedb4428
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-01-06 15:01:59 -0300

    Removida annotation desnecessária.

--- src/main/java/api/configs/data/DatabaseConfig.java
+++ src/main/java/api/configs/data/DatabaseConfig.java
@@ -1,11 +1,9 @@
 package api.configs.data;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 @Configuration
 @EnableR2dbcAuditing
-@RequiredArgsConstructor
 public class DatabaseConfig {
 }
