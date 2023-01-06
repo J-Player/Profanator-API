43551336f694f9dac57069890882ae4fcfe180ae
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-01-06 15:15:09 -0300

    Classes de testes de integração atualizados.

--- src/test/java/api/integration/IngredientControllerIT.java
+++ src/test/java/api/integration/IngredientControllerIT.java
@@ -1,89 +1,182 @@
 package api.integration;
 
+import api.configs.BlockHoundTest;
 import api.domains.Ingredient;
+import api.domains.Proficiency;
 import api.domains.dtos.IngredientDTO;
 import api.integration.annotation.IntegrationTest;
-import api.repositories.IngredientRepository;
+import api.mappers.IngredientMapper;
 import api.services.ItemService;
+import api.services.ProficiencyService;
 import api.util.IngredientCreator;
-import api.util.ItemCreator;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Sort;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.blockhound.BlockHound;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static api.integration.constraint.IntegrationConstraint.ADMIN_USER;
+import static api.integration.constraint.IntegrationConstraint.PATH_INGREDIENTS;
+import static api.util.ItemCreator.item;
+import static api.util.ProficiencyCreator.proficiency;
 
 @IntegrationTest
 @DisplayName("Ingredient Controller Integration Test")
 class IngredientControllerIT {
 
-    private static final String ADMIN_USER = "admin";
-    private static final String PATH = "/ingredients";
-
     @Autowired
     private WebTestClient client;
 
-    @MockBean
-    private ItemService itemService;
+    private static Ingredient ingredient;
+    private static final IngredientDTO ingredientDTO = IngredientCreator.ingredientDTO();
+    private static final IngredientDTO invalidIngredientDTO = IngredientCreator.invalidIngredientDTO();
 
-    @MockBean
-    private IngredientRepository ingredientRepository;
+    @BeforeAll
+    static void beforeAll(@Autowired ProficiencyService proficiencyService, @Autowired ItemService itemService) {
+        BlockHound.install();
+        proficiencyService.save(proficiency().withId(null))
+                .map(Proficiency::getName)
+                .flatMapMany(proficiency -> Flux.just(
+                        item()
+                                .withId(null)
+                                .withProficiency(proficiency)
+                                .withName(ingredientDTO.getProduct()),
+                        item()
+                                .withId(null)
+                                .withProficiency(proficiency)
+                                .withName(ingredientDTO.getName())))
+                .flatMap(itemService::save)
+                .blockLast();
+    }
 
-    private final Ingredient ingredient = IngredientCreator.ingredient();
-    private final IngredientDTO ingredientDTO = IngredientCreator.ingredientDTO();
-    private final IngredientDTO invalidIngredientDTO = IngredientCreator.invalidIngredientDTO();
-
-    @BeforeEach
-    void setUp() {
-        BDDMockito.when(ingredientRepository.findById(any(UUID.class))).thenReturn(Mono.just(ingredient));
-        BDDMockito.when(ingredientRepository.findByProductAndNameAllIgnoreCase(anyString(), anyString())).thenReturn(Mono.just(ingredient));
-        BDDMockito.when(ingredientRepository.findByProductIgnoreCase(anyString(), any(Sort.class))).thenReturn(Flux.just(ingredient));
-        BDDMockito.when(ingredientRepository.findAll(any(Sort.class))).thenReturn(Flux.just(ingredient));
-        BDDMockito.when(ingredientRepository.save(any(Ingredient.class))).thenReturn(Mono.just(ingredient));
-        BDDMockito.when(ingredientRepository.delete(any(Ingredient.class))).thenReturn(Mono.empty());
-        BDDMockito.when(itemService.findByName(anyString())).thenReturn(Mono.just(ItemCreator.item()));
+    @AfterAll
+    static void afterAll(@Autowired ProficiencyService proficiencyService, @Autowired ItemService itemService) {
+        itemService.findByName(ingredient.getProduct())
+                .mergeWith(itemService.findByName(ingredient.getName()))
+                .flatMap(item -> itemService.delete(item.getId()))
+                .concatWith(proficiencyService.findByName(proficiency().getName())
+                        .map(Proficiency::getId)
+                        .flatMap(proficiencyService::delete))
+                .blockLast();
     }
 
     @Test
+    @Order(0)
+    @DisplayName("[BlockHound] Check if BlockHound is working")
+    void blockHoundWorks() {
+        BlockHoundTest.test();
+    }
+
+    @Test
+    @Order(1)
+    @WithUserDetails(ADMIN_USER)
+    @DisplayName("save | Returns a ingredient when successful")
+    void save() {
+        ingredient = client.post()
+                .uri(PATH_INGREDIENTS)
+                .bodyValue(ingredientDTO)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Ingredient.class)
+                .returnResult()
+                .getResponseBody();
+    }
+
+    @Test
+    @Order(2)
+    @WithUserDetails(ADMIN_USER)
+    @DisplayName("save | Returns 400 error when invalid ingredient")
+    void save_ReturnsError_WhenInvalidIngredient() {
+        client.post()
+                .uri(PATH_INGREDIENTS)
+                .bodyValue(invalidIngredientDTO)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    @Order(3)
+    @WithUserDetails
+    @DisplayName("save | Returns 403 error when forbidden user")
+    void save_ReturnsError_WhenForbiddenUser() {
+        client.post()
+                .uri(PATH_INGREDIENTS)
+                .bodyValue(ingredientDTO)
+                .exchange()
+                .expectStatus().isForbidden();
+    }
+
+    @Test
+    @Order(4)
+    @WithUserDetails(ADMIN_USER)
+    @DisplayName("update | Returns status 204 (no content) when successful")
+    void update() {
+        client.put()
+                .uri(PATH_INGREDIENTS.concat("/{id}"), ingredient.getId())
+                .bodyValue(IngredientMapper.INSTANCE.toIngredientDTO(ingredient.withQuantity(4)))
+                .exchange()
+                .expectStatus().isNoContent();
+    }
+
+    @Test
+    @Order(5)
+    @WithUserDetails(ADMIN_USER)
+    @DisplayName("update | Returns 400 error when invalid ingredient")
+    void update_ReturnsError_WhenInvalidIngredient() {
+        client.put()
+                .uri(PATH_INGREDIENTS.concat("/{id}"), ingredient.getId())
+                .bodyValue(invalidIngredientDTO)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    @Order(6)
+    @WithUserDetails
+    @DisplayName("update | Returns 403 error when forbidden user")
+    void update_ReturnsError_WhenForbiddenUser() {
+        client.put()
+                .uri(PATH_INGREDIENTS.concat("/{id}"), ingredient.getId())
+                .bodyValue(ingredientDTO)
+                .exchange()
+                .expectStatus().isForbidden();
+    }
+
+    @Test
+    @Order(7)
     @WithUserDetails
     @DisplayName("findById | Returns a ingredient when successful")
     void findById() {
-        client.get()
-                .uri(PATH.concat("/{id}"), ingredient.getId())
+        ingredient = client.get()
+                .uri(PATH_INGREDIENTS.concat("/{id}"), ingredient.getId())
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(Ingredient.class)
-                .isEqualTo(ingredient);
+                .returnResult()
+                .getResponseBody();
     }
 
     @Test
+    @Order(8)
     @WithUserDetails
     @DisplayName("findById | Returns 404 error when not found")
     void findById_ReturnsError_WhenNotFound() {
-        BDDMockito.when(ingredientRepository.findById(any(UUID.class))).thenReturn(Mono.empty());
         client.get()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
+                .uri(PATH_INGREDIENTS.concat("/{id}"), UUID.randomUUID())
                 .exchange()
                 .expectStatus().isNotFound();
     }
 
     @Test
+    @Order(9)
     @WithUserDetails
     @DisplayName("findByItemAndIngredient | Returns a ingredient when successful")
     void findByItemAndIngredient() {
         client.get()
-                .uri(PATH.concat("/{item}/{ingredient}"), ingredient.getProduct(), ingredient.getName())
+                .uri(PATH_INGREDIENTS.concat("/{item}/{ingredient}"), ingredient.getProduct(), ingredient.getName())
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(Ingredient.class)
@@ -91,36 +184,36 @@ class IngredientControllerIT {
     }
 
     @Test
+    @Order(10)
     @WithUserDetails
     @DisplayName("findByItemAndIngredient | Returns 404 error when not found")
     void findByItemAndIngredient_ReturnsError_WhenNotFound() {
-        BDDMockito.when(ingredientRepository.findByProductAndNameAllIgnoreCase(anyString(), anyString()))
-                .thenReturn(Mono.empty());
         client.get()
-                .uri(PATH.concat("/{item}/{ingredient}"), "randomProduct", "randomName")
+                .uri(PATH_INGREDIENTS.concat("/{item}/{ingredient}"), "randomProduct", "randomName")
                 .exchange()
                 .expectStatus().isNotFound();
     }
 
     @Test
+    @Order(11)
     @WithUserDetails
     @DisplayName("listAll | Returns all ingredients when successful")
     void listAll() {
         client.get()
-                .uri(builder -> builder.path(PATH.concat("/all")).build())
+                .uri(builder -> builder.path(PATH_INGREDIENTS.concat("/all")).build())
                 .exchange()
                 .expectStatus().isOk()
                 .expectBodyList(Ingredient.class)
-                .hasSize(1)
                 .contains(ingredient);
     }
 
     @Test
+    @Order(12)
     @WithUserDetails
     @DisplayName("listAllByItem | Returns all ingredients of a item when successful")
     void listAllByItem() {
         client.get()
-                .uri(builder -> builder.path(PATH.concat("/all"))
+                .uri(builder -> builder.path(PATH_INGREDIENTS.concat("/all"))
                         .queryParam("item", ingredient.getProduct()).build())
                 .exchange()
                 .expectStatus().isOk()
@@ -130,101 +223,34 @@ class IngredientControllerIT {
     }
 
     @Test
-    @WithUserDetails(ADMIN_USER)
-    @DisplayName("save | Returns a ingredient when successful")
-    void save() {
-        client.post()
-                .uri(PATH)
-                .bodyValue(ingredientDTO)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectBody(Ingredient.class)
-                .isEqualTo(ingredient);
-    }
-
-    @Test
-    @WithUserDetails(ADMIN_USER)
-    @DisplayName("save | Returns 400 error when invalid ingredient")
-    void save_ReturnsError_WhenInvalidIngredient() {
-        client.post()
-                .uri(PATH)
-                .bodyValue(invalidIngredientDTO)
-                .exchange()
-                .expectStatus().isBadRequest();
-    }
-
-    @Test
-    @WithUserDetails
-    @DisplayName("save | Returns 403 error when forbidden user")
-    void save_ReturnsError_WhenForbiddenUser() {
-        client.post()
-                .uri(PATH)
-                .bodyValue(ingredientDTO)
-                .exchange()
-                .expectStatus().isForbidden();
-    }
-
-    @Test
-    @WithUserDetails(ADMIN_USER)
-    @DisplayName("update | Returns status 204 (no content) when successful")
-    void update() {
-        client.put()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
-                .bodyValue(ingredientDTO)
-                .exchange()
-                .expectStatus().isNoContent();
-    }
-
-    @Test
-    @WithUserDetails(ADMIN_USER)
-    @DisplayName("update | Returns 400 error when invalid ingredient")
-    void update_ReturnsError_WhenInvalidIngredient() {
-        BDDMockito.when(ingredientRepository.findById(any(UUID.class))).thenReturn(Mono.empty());
-        client.put()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
-                .bodyValue(invalidIngredientDTO)
-                .exchange()
-                .expectStatus().isBadRequest();
-    }
-
-    @Test
-    @WithUserDetails
-    @DisplayName("update | Returns 403 error when forbidden user")
-    void update_ReturnsError_WhenForbiddenUser() {
-        client.put()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
-                .bodyValue(ingredientDTO)
-                .exchange()
-                .expectStatus().isForbidden();
-    }
-
-    @Test
+    @Order(13)
     @WithUserDetails(ADMIN_USER)
     @DisplayName("delete | Returns status 204 (no content) when successful")
     void delete() {
         client.delete()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
+                .uri(PATH_INGREDIENTS.concat("/{id}"), ingredient.getId())
                 .exchange()
                 .expectStatus().isNoContent();
     }
 
     @Test
+    @Order(14)
     @WithUserDetails
     @DisplayName("delete | Returns 403 error when forbidden user")
     void delete_ReturnsError_WhenForbiddenUser() {
         client.delete()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
+                .uri(PATH_INGREDIENTS.concat("/{id}"), ingredient.getId())
                 .exchange()
                 .expectStatus().isForbidden();
     }
 
     @Test
+    @Order(15)
     @WithUserDetails(ADMIN_USER)
     @DisplayName("delete | Returns 404 error when not found")
     void delete_ReturnsError_WhenNotFound() {
-        BDDMockito.when(ingredientRepository.findById(any(UUID.class))).thenReturn(Mono.empty());
         client.delete()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
+                .uri(PATH_INGREDIENTS.concat("/{id}"), UUID.randomUUID())
                 .exchange()
                 .expectStatus().isNotFound();
     }
--- src/test/java/api/integration/ItemControllerIT.java
+++ src/test/java/api/integration/ItemControllerIT.java
@@ -1,231 +1,242 @@
 package api.integration;
 
+import api.configs.BlockHoundTest;
 import api.domains.Item;
+import api.domains.Proficiency;
 import api.domains.dtos.ItemDTO;
 import api.integration.annotation.IntegrationTest;
-import api.repositories.ItemRepository;
+import api.mappers.ItemMapper;
 import api.services.ProficiencyService;
 import api.util.ItemCreator;
-import api.util.ProficiencyCreator;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Sort;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import reactor.blockhound.BlockHound;
 
 import java.util.UUID;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static api.integration.constraint.IntegrationConstraint.ADMIN_USER;
+import static api.integration.constraint.IntegrationConstraint.PATH_ITEMS;
 
 @IntegrationTest
 @DisplayName("Item Controller Integration Test")
 class ItemControllerIT {
 
-    private static final String ADMIN_USER = "admin";
-    private static final String PATH = "/items";
-
     @Autowired
     private WebTestClient client;
 
-    @MockBean
-    private ProficiencyService proficiencyService;
+    private static Item item;
+    private static final ItemDTO itemDTO = ItemCreator.itemDTO();
+    private static final ItemDTO invalidItemDTO = ItemCreator.invalidItemDTO();
 
-    @MockBean
-    private ItemRepository itemRepository;
+    @BeforeAll
+    static void beforeAll(@Autowired ProficiencyService proficiencyService) {
+        BlockHound.install();
+        proficiencyService.save(Proficiency.builder()
+                        .name(itemDTO.getProficiency())
+                        .build())
+                .map(Proficiency::getName)
+                .block();
+    }
 
-    private final Item item = ItemCreator.item();
-    private final ItemDTO itemDTO = ItemCreator.itemDTO();
-    private final ItemDTO invalidItemDTO = ItemCreator.invalidItemDTO();
-
-    @BeforeEach
-    void setUp() {
-        BDDMockito.when(itemRepository.findById(any(UUID.class))).thenReturn(Mono.just(item));
-        BDDMockito.when(itemRepository.findByNameIgnoreCase(anyString())).thenReturn(Mono.just(item));
-        BDDMockito.when(itemRepository.findAllByProficiencyIgnoreCase(anyString(), any(Sort.class))).thenReturn(Flux.just(item));
-        BDDMockito.when(itemRepository.findAll(any(Sort.class))).thenReturn(Flux.just(item));
-        BDDMockito.when(itemRepository.save(any(Item.class))).thenReturn(Mono.just(item));
-        BDDMockito.when(itemRepository.delete(any(Item.class))).thenReturn(Mono.empty());
-        BDDMockito.when(proficiencyService.findByName(anyString()))
-                .thenReturn(Mono.just(ProficiencyCreator.proficiency()));
+    @AfterAll
+    static void AfterAll(@Autowired ProficiencyService proficiencyService) {
+        proficiencyService.findByName(item.getProficiency())
+                .map(Proficiency::getId)
+                .flatMap(proficiencyService::delete)
+                .block();
     }
 
     @Test
-    @WithUserDetails
-    @DisplayName("findById | Returns a item when successful")
-    void findById() {
-        client.get()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(Item.class)
-                .isEqualTo(item);
+    @Order(0)
+    @DisplayName("[BlockHound] Check if BlockHound is working")
+    void blockHoundWorks() {
+        BlockHoundTest.test();
     }
 
     @Test
-    @WithUserDetails
-    @DisplayName("findById | Returns 404 error when not found")
-    void findById_ReturnsError_WhenNotFound() {
-        BDDMockito.when(itemRepository.findById(any(UUID.class))).thenReturn(Mono.empty());
-        client.get()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
-                .exchange()
-                .expectStatus().isNotFound();
-    }
-
-    @Test
-    @WithUserDetails
-    @DisplayName("findByName | Returns a item when successful")
-    void findByName() {
-        client.get()
-                .uri(builder -> builder.path(PATH).queryParam("name", item.getName()).build())
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(Item.class)
-                .isEqualTo(item);
-
-    }
-
-    @Test
-    @WithUserDetails
-    @DisplayName("findByName | Returns 404 error when not found")
-    void findByName_ReturnsError_WhenNotFound() {
-        BDDMockito.when(itemRepository.findByNameIgnoreCase(anyString())).thenReturn(Mono.empty());
-        client.get()
-                .uri(builder -> builder.path(PATH).queryParam("name", "randomName").build())
-                .exchange()
-                .expectStatus().isNotFound();
-    }
-
-    @Test
-    @WithUserDetails
-    @DisplayName("listAll | Returns all items when successful")
-    void listAll() {
-        client.get()
-                .uri(builder -> builder.path(PATH.concat("/all")).build())
-                .exchange()
-                .expectStatus().isOk()
-                .expectBodyList(Item.class)
-                .hasSize(1)
-                .contains(item);
-    }
-
-    @Test
-    @WithUserDetails
-    @DisplayName("listAllByProficiency | Returns all items of a proficiency when successful")
-    void listAllByProficiency() {
-        client.get()
-                .uri(builder -> builder.path(PATH.concat("/all"))
-                        .queryParam("proficiency", item.getProficiency()).build())
-                .exchange()
-                .expectStatus().isOk()
-                .expectBodyList(Item.class)
-                .hasSize(1)
-                .contains(item);
-    }
-
-    @Test
+    @Order(1)
     @WithUserDetails(ADMIN_USER)
     @DisplayName("save | Returns a item when successful")
     void save() {
-        client.post()
-                .uri(PATH)
+        item = client.post()
+                .uri(PATH_ITEMS)
                 .bodyValue(itemDTO)
                 .exchange()
                 .expectStatus().isCreated()
                 .expectBody(Item.class)
-                .isEqualTo(item);
+                .returnResult()
+                .getResponseBody();
     }
 
     @Test
+    @Order(2)
     @WithUserDetails(ADMIN_USER)
     @DisplayName("save | Returns 400 error when invalid item")
     void save_ReturnsError_WhenInvalidItem() {
         client.post()
-                .uri(PATH)
+                .uri(PATH_ITEMS)
                 .bodyValue(invalidItemDTO)
                 .exchange()
                 .expectStatus().isBadRequest();
     }
 
     @Test
+    @Order(3)
     @WithUserDetails
     @DisplayName("save | Returns 403 error when forbidden user")
     void save_ReturnsError_WhenForbiddenUser() {
         client.post()
-                .uri(PATH)
+                .uri(PATH_ITEMS)
                 .bodyValue(itemDTO)
                 .exchange()
                 .expectStatus().isForbidden();
     }
 
     @Test
+    @Order(4)
     @WithUserDetails(ADMIN_USER)
     @DisplayName("update | Returns status 204 (no content) when successful")
     void update() {
         client.put()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
-                .bodyValue(itemDTO)
+                .uri(PATH_ITEMS.concat("/{id}"), item.getId())
+                .bodyValue(ItemMapper.INSTANCE.toItemDTO(item
+                        .withName("New_Name")
+                        .withQtByProduction(3)))
                 .exchange()
                 .expectStatus().isNoContent();
     }
 
     @Test
+    @Order(5)
     @WithUserDetails(ADMIN_USER)
     @DisplayName("update | Returns 400 error when invalid item")
     void update_ReturnsError_WhenInvalidItem() {
-        BDDMockito.when(itemRepository.findById(any(UUID.class))).thenReturn(Mono.empty());
         client.put()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
+                .uri(PATH_ITEMS.concat("/{id}"), item.getId())
                 .bodyValue(invalidItemDTO)
                 .exchange()
                 .expectStatus().isBadRequest();
     }
 
     @Test
+    @Order(6)
     @WithUserDetails
     @DisplayName("update | Returns 403 error when forbidden user")
     void update_ReturnsError_WhenForbiddenUser() {
         client.put()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
+                .uri(PATH_ITEMS.concat("/{id}"), item.getId())
                 .bodyValue(itemDTO)
                 .exchange()
                 .expectStatus().isForbidden();
     }
 
     @Test
+    @Order(7)
+    @WithUserDetails
+    @DisplayName("findById | Returns a item when successful")
+    void findById() {
+        item = client.get()
+                .uri(PATH_ITEMS.concat("/{id}"), item.getId())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Item.class)
+                .returnResult()
+                .getResponseBody();
+    }
+
+    @Test
+    @Order(8)
+    @WithUserDetails
+    @DisplayName("findById | Returns 404 error when not found")
+    void findById_ReturnsError_WhenNotFound() {
+        client.get()
+                .uri(PATH_ITEMS.concat("/{id}"), UUID.randomUUID())
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @Order(9)
+    @WithUserDetails
+    @DisplayName("findByName | Returns a item when successful")
+    void findByName() {
+        client.get()
+                .uri(builder -> builder.path(PATH_ITEMS).queryParam("name", item.getName()).build())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Item.class)
+                .isEqualTo(item);
+    }
+
+    @Test
+    @Order(10)
+    @WithUserDetails
+    @DisplayName("findByName | Returns 404 error when not found")
+    void findByName_ReturnsError_WhenNotFound() {
+        client.get()
+                .uri(builder -> builder.path(PATH_ITEMS).queryParam("name", "randomName").build())
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @Order(11)
+    @WithUserDetails
+    @DisplayName("listAll | Returns all items when successful")
+    void listAll() {
+        client.get()
+                .uri(builder -> builder.path(PATH_ITEMS.concat("/all")).build())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Item.class)
+                .contains(item);
+    }
+
+    @Test
+    @Order(12)
+    @WithUserDetails
+    @DisplayName("listAllByProficiency | Returns all items of a proficiency when successful")
+    void listAllByProficiency() {
+        client.get()
+                .uri(builder -> builder.path(PATH_ITEMS.concat("/all"))
+                        .queryParam("proficiency", item.getProficiency()).build())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Item.class)
+                .contains(item);
+    }
+
+    @Test
+    @Order(13)
     @WithUserDetails(ADMIN_USER)
     @DisplayName("delete | Returns status 204 (no content) when successful")
     void delete() {
         client.delete()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
+                .uri(PATH_ITEMS.concat("/{id}"), item.getId())
                 .exchange()
                 .expectStatus().isNoContent();
     }
 
     @Test
+    @Order(14)
     @WithUserDetails
     @DisplayName("delete | Returns 403 error when forbidden user")
     void delete_ReturnsError_WhenForbiddenUser() {
         client.delete()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
+                .uri(PATH_ITEMS.concat("/{id}"), item.getId())
                 .exchange()
                 .expectStatus().isForbidden();
     }
 
     @Test
+    @Order(15)
     @WithUserDetails(ADMIN_USER)
     @DisplayName("delete | Returns 404 error when not found")
     void delete_ReturnsError_WhenNotFound() {
-        BDDMockito.when(itemRepository.findById(any(UUID.class))).thenReturn(Mono.empty());
         client.delete()
-                .uri(PATH.concat("/{id}"), UUID.randomUUID())
+                .uri(PATH_ITEMS.concat("/{id}"), UUID.randomUUID())
                 .exchange()
                 .expectStatus().isNotFound();
     }
