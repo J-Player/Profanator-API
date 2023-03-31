c97c6e91718acdb67f3a9e9a6d8062a78dd4808d
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-03-31 17:28:26 -0300

    Pequenas refatorações realizadas.

--- src/test/java/api/integration/annotation/IntegrationTest.java
+++ src/test/java/api/integration/annotation/IntegrationTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.annotation.AliasFor;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.*;
@@ -12,16 +11,9 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Inherited
-@ActiveProfiles
+@ActiveProfiles("test")
 @AutoConfigureWebTestClient
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public @interface IntegrationTest {
-
-    @AliasFor(annotation = ActiveProfiles.class, attribute = "profiles")
-    String[] activeProfiles() default {"test"};
-
-    @AliasFor(annotation = TestMethodOrder.class, attribute = "value")
-    Class<? extends MethodOrderer> methodOrder() default MethodOrderer.OrderAnnotation.class;
-
 }
