a9feb8747cb02b1207101dd8e9d905e1977e65d4
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-04-07 10:51:50 -0300

    Annotation Import foi adicionado e annotation TestMethodOrder alterado.

--- src/test/java/api/integration/annotation/IntegrationTest.java
+++ src/test/java/api/integration/annotation/IntegrationTest.java
@@ -1,9 +1,11 @@
 package api.integration.annotation;
 
+import api.configs.security.SecurityConfig;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.*;
@@ -13,7 +15,8 @@ import java.lang.annotation.*;
 @Inherited
 @ActiveProfiles("test")
 @AutoConfigureWebTestClient
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Import({SecurityConfig.class})
+@TestMethodOrder(MethodOrderer.DisplayName.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public @interface IntegrationTest {
 }
